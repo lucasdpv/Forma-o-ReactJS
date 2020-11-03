08d93a800168b9e88f64f69e51a1da6d40e594ce
Lucas Vieira <33061562+lucasdpv@users.noreply.github.com>
2020-11-03 20:15:13 -0300

    criaçaõ da classe cliente

--- JavaScript Programando a Orientação a Objetos/index.js
+++ JavaScript Programando a Orientação a Objetos/index.js
@@ -5,4 +5,10 @@
      saldo;
  }
 
- 
+const cliente1 = new Cliente();
+cliente1.nome = "Lucas";
+cliente1.cpf = "00701833333"
+cliente1.agencia = "000001"
+cliente1.saldo = 123;
+
+console.log(cliente1);
