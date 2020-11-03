cf2d6ee3edebdb26f80f12ea021658ed083addb8
Lucas Vieira <33061562+lucasdpv@users.noreply.github.com>
2020-11-03 20:34:40 -0300

    criando classe ContaCorrente

--- JavaScript Programando a Orientação a Objetos/index.js
+++ JavaScript Programando a Orientação a Objetos/index.js
@@ -1,14 +1,25 @@
- class Cliente {
-     nome;
-     cpf;
-     agencia;
-     saldo;
- }
+class Cliente {
+  nome;
+  cpf;
+}
+
+class ContaCorrente {
+    agencia;
+    saldo;
+}
 
 const cliente1 = new Cliente();
 cliente1.nome = "Lucas";
-cliente1.cpf = "00701833333"
-cliente1.agencia = "000001"
-cliente1.saldo = 123;
+cliente1.cpf = 123123322;
+
+
+const cliente2 = new Cliente();
+cliente2.nome = "Stephanie";
+cliente2.cpf = 123123321;
+
+const contaCorrente1 = new ContaCorrente();
+contaCorrente1.saldo = 0;
+contaCorrente1.agencia = 1001;
 
 console.log(cliente1);
+console.log(cliente2);
