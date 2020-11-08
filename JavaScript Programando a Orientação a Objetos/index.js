37064cbbb0f927e8395fa44ec86476d25d3d4fe7
Lucas Vieira <33061562+lucasdpv@users.noreply.github.com>
2020-11-08 12:20:35 -0300

    configurando package.json e criando modulos

--- JavaScript Programando a Orientação a Objetos/index.js
+++ JavaScript Programando a Orientação a Objetos/index.js
@@ -1,33 +1,13 @@
-class Cliente {
-  nome;
-  cpf;
-}
-
-class ContaCorrente {
-  #agencia;
-  #saldo;
-
-  sacar(valor) {
-    if (this.#saldo >= valor) {
-      this.#saldo -= valor;
-    }
-  }
-
-  depositar(valor) {
-    if (valor > 0) {
-      this.#saldo += valor;
-    }
-  }
-}
+import { ContaCorrente } from  "./ContaCorrente.js"
+import { Cliente } from "./Cliente.js"
 
 const cliente1 = new Cliente();
 cliente1.nome = "Lucas";
 cliente1.cpf = 123123322;
 
 const contaCorrente1 = new ContaCorrente();
-contaCorrente1.saldo = 0;
 contaCorrente1.agencia = 1001;
 contaCorrente1.depositar(100);
 contaCorrente1.sacar(50);
- 
-console.log(contaCorrente1);
+
+console.log(contaCorrente1.getSaldo());
